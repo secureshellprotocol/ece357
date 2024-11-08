5276a9e3b59f80c63a6d158517b3c9ca8b13fcc1
JAMES RYAN <james.paul9889@gmail.com>
2024-11-08 13:01:37 -0500

    removed most macros on error reporting
    cleanup is now the responsibility of each function call

--- hw4/catgrepmore/macros.h
+++ hw4/catgrepmore/macros.h
@@ -7,19 +7,9 @@
 #define EXIT_OK             0
 #define EXIT_FAIL           -1
 
-// Macro to report an error to 'stderr' AND CLOSE AS A FAILURE.
-// Include a trailing '%s' in 'message' to also report errno.
-#define ERR_CLOSE(message, ...) \
-    fprintf(stderr, message"\n", ## __VA_ARGS__, strerror(errno));\
-    return EXIT_FAIL;
-
 // Macro to report an error to 'stderr' and continue.
-// Include a trailing '%s' in 'message' to also report errno.
-#define ERR_CONT(message, ...) \
-    fprintf(stderr, message"\n", ## __VA_ARGS__, strerror(errno));
-
-// Same as ERR_CONT macro, however, theres NO NewLine.
-#define ERR_CONT_NONL(message, ...) \
+// We are NOT responsible for cleanup.
+#define ERR(message, ...) \
     fprintf(stderr, message, ## __VA_ARGS__, strerror(errno));
 
 #endif
